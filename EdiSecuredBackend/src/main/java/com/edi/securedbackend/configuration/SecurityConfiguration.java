24de26bd9d8dbacacf4019c9d4fbac8c1e54339c
Andrea Galmuzzi <andrea.galmuzzi01@universitadipavia.it>
2023-06-01 13:53:33 +0200

    Adding HTTPS support

--- EdiSecuredBackend/src/main/java/com/edi/securedbackend/configuration/SecurityConfiguration.java
+++ EdiSecuredBackend/src/main/java/com/edi/securedbackend/configuration/SecurityConfiguration.java
@@ -23,52 +23,53 @@ import java.util.Collections;
 @RequiredArgsConstructor
 public class SecurityConfiguration {
 
-    private final JwtAuthenticationFilter jwtAuthFilter;
-    private final AuthenticationProvider authenticationProvider;
-    private final LogoutHandler logoutHandler;
+	private final JwtAuthenticationFilter jwtAuthFilter;
+	private final AuthenticationProvider authenticationProvider;
+	private final LogoutHandler logoutHandler;
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .cors(Customizer.withDefaults())
-                .csrf()
-                .disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/auth/**")
-                .permitAll()
-                //.requestMatchers("/books*")
-                //.permitAll()
-                //.requestMatchers("/loans")
-                //.permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .sessionManagement()
-                /* Use stateless sessions since we are using JWT tokens. */
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                /* Use the custom authentication provider. */
-                .authenticationProvider(authenticationProvider)
-                /* Add the JWT authentication filter before the username/password filter. */
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-                .logout()
-                /* URL to logout users. */
-                .logoutUrl("auth/logout")
-                /* Clear the authentication when the user logs out. */
-                .logoutSuccessHandler(((request, response, authentication) -> SecurityContextHolder.clearContext()));
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+				.cors(Customizer.withDefaults())
+				.csrf()
+				.disable()
+				.requiresChannel(channel -> channel.anyRequest().requiresSecure())
+				.authorizeRequests()
+				.requestMatchers("/auth/**")
+				.permitAll()
+				//.requestMatchers("/books*")
+				//.permitAll()
+				//.requestMatchers("/loans")
+				//.permitAll()
+				.anyRequest()
+				.authenticated()
+				.and()
+				.sessionManagement()
+				/* Use stateless sessions since we are using JWT tokens. */
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and()
+				/* Use the custom authentication provider. */
+				.authenticationProvider(authenticationProvider)
+				/* Add the JWT authentication filter before the username/password filter. */
+				.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+				.logout()
+				/* URL to logout users. */
+				.logoutUrl("auth/logout")
+				/* Clear the authentication when the user logs out. */
+				.logoutSuccessHandler(((request, response, authentication) -> SecurityContextHolder.clearContext()));
 
-        return http.build();
-    }
+		return http.build();
+	}
 
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(Collections.singletonList("*"));
-        corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
-        corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfiguration);
-        return source;
-    }
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowedOrigins(Collections.singletonList("*"));
+		corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
+		corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfiguration);
+		return source;
+	}
 
 }
